e714ac45f4acc0d269a6ff8cfefa7da8e52909be
mike-papa <73196026+mike-papa@users.noreply.github.com>
2024-01-06 02:41:35 +0100

    Add basic map

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import MyGeoJSONMap from "./components/leaflet_maps/MapComponent";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -31,6 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <MyGeoJSONMap />
     </>
   );
 }
